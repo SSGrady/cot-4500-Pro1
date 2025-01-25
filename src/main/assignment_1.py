1c249f1a30251c269f67ccbae6a53c47a404b58e
Steven Grady <stevenagrady@gmail.com>
2025-01-25 14:51:47 -0500

    Implement and print Three-digit chopping for hw1

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -2,9 +2,10 @@
 import math
 
 # 1. Double Precision Calculation
-def double_precision():
+# Input: binary a truncated 64-bit string in IEEE 754 format
+hw_binary = "010000000111111010111001"
+def double_precision(binary):
     BIAS = 1023
-    binary = "010000000111111010111001"
 
     sign = int(binary[0])
     exponent_bits = binary[1:12]
@@ -22,4 +23,25 @@ def double_precision():
 
     return value
 
-print(f"1) {double_precision():.5f}\n") #1) 491.56250
+exact_value = double_precision(hw_binary)
+
+
+
+# 2. Three-Digit Chopping
+def three_digit_chopping(value):
+    if value == 0:
+        return 0.0000
+    magnitude = math.floor(math.log10(abs(value)))
+    scale_factor = 10 ** (3 - 1 - magnitude)
+    scaled_value = value * scale_factor
+    chopped = int(scaled_value) / scale_factor
+    return float(f"{chopped:.5f}")
+
+
+chopped_value = three_digit_chopping(exact_value)
+
+def print_hw1():
+    print(f"1) {exact_value:.5f}\n") #1) 491.56250
+    print(f"2) {chopped_value:.5f}\n") #2) 491.00000
+
+print_hw1()
